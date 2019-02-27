794b3858123894ee3acbeab972c232b03e49557e
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-02-27 02:14:58 +0100

    vypis jmena

--- cviceni-2.php
+++ cviceni-2.php
@@ -1,12 +1,4 @@
-Den v tydnu: $den
-<?php
-$a = 4;
-$b = 5;
-$obdelnikobsah = $a * $b
-echo “Obsah obdelníku o stranách" . $a "a " . $b . "je: " $obsahobdelník;
-/**
- * Created by PhpStorm.
- * User: juand
- * Date: 21.02.2019
- * Time: 19:14
- */
+
+<h1>Nadpis</h1>
+<?php echo '<p>Odstavec 1</p>'; ?>
+<p>Odstavec 2</p>
