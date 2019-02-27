becd30c61cfffabced782fdc2cebf7cc8ee60890
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-02-27 02:59:19 +0100

    vypis jmena

---
+++ pythagoras.php
@@ -0,0 +1,14 @@
+<?php
+<h1>Nadpis</h1>
+$a = 4
+$b = 5
+$obsah = $a * $b
+
+<?php echo '<p>Obdelnich o stranach $a . a . $b </p>'; ?>
+<p>Odstavec 2</p>
+/**
+ * Created by PhpStorm.
+ * User: juand
+ * Date: 27.02.2019
+ * Time: 2:56
+ */
